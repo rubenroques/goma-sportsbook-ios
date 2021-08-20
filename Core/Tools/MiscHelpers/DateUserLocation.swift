3b33ec588aa7848fcb627cd9f4e4b7e77945db24
André Lascas <andrelascas@MacBook-Pro-de-Andre.local>
2021-08-20 11:01:47 +0100

    initial branch commit

---
+++ Core/Tools/MiscHelpers/DateUserLocation.swift
@@ -0,0 +1,15 @@
+//
+//  DateUserLocation.swift
+//  Sportsbook
+//
+//  Created by André Lascas on 20/08/2021.
+//
+
+import Foundation
+
+struct DateUserLocation {
+
+    func dateLocationFormat{
+        
+    }
+}
