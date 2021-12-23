e7eae8257a5ff0592531504e3c7f8d8a95af176a
André Lascas <andrelascas@MacBook-Pro-de-Andre.local>
2021-12-23 16:54:40 +0000

    added empty state cards for my games and my competitions list

---
+++ Core/Screens/Home/PreLive/Cells/EmptyCardTableViewCell.swift
@@ -0,0 +1,50 @@
+//
+//  EmptyCardTableViewCell.swift
+//  Sportsbook
+//
+//  Created by André Lascas on 23/12/2021.
+//
+
+import UIKit
+
+class EmptyCardTableViewCell: UITableViewCell {
+
+    @IBOutlet var containerView: UIView!
+    @IBOutlet var iconImageView: UIImageView!
+    @IBOutlet var descriptionLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        self.setup()
+
+        self.setupWithTheme()
+    }
+
+    func setup() {
+        self.iconImageView.image = UIImage(named: "warning_alert_icon")
+
+        self.descriptionLabel.text = "Lorem ipsum"
+        self.descriptionLabel.numberOfLines = 0
+    }
+
+    func setupWithTheme() {
+
+        self.backgroundColor = .clear
+        self.contentView.backgroundColor = .clear
+
+        self.containerView.backgroundColor = UIColor.App.secondaryBackground
+        self.containerView.layer.cornerRadius = CornerRadius.modal
+        self.containerView.layer.masksToBounds = true
+
+        self.iconImageView.backgroundColor = .clear
+
+        self.descriptionLabel.textColor = UIColor.App.headingMain
+        self.descriptionLabel.font = AppFont.with(type: .semibold, size: 14)
+    }
+
+    func setDescription(text: String) {
+        self.descriptionLabel.text = text
+    }
+
+}
