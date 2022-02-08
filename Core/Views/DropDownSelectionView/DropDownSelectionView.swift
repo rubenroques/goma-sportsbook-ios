925e359a771c5ad62bb28efdee1811f9fdaa9f03
Teresa <teresa@gomadevelopment.pt>
2022-02-08 18:14:31 +0000

    no message

--- Core/Views/DropDownSelectionView/DropDownSelectionView.swift
+++ Core/Views/DropDownSelectionView/DropDownSelectionView.swift
@@ -1,20 +1,492 @@
 //
 //  DropDownSelectionView.swift
-//  ShowcaseProd
+//  Sportsbook
 //
-//  Created by Teresa on 03/02/2022.
+//  Created by Teresa on 08/02/2022.
 //
 
-import SwiftUI
+import UIKit
+import CombineCocoa
+import Combine
 
-struct DropDownSelectionView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+class DropDownSelectionView: NibView {
+
+    @IBOutlet private weak var containerView: UIView!
+    @IBOutlet private weak var headerPlaceholderLabel: UILabel!
+
+    @IBOutlet private weak var headerLabel: UILabel! // swiftlint:disable:this private_outlet
+
+    @IBOutlet private weak var textLabel: UILabel!
+    
+    @IBOutlet private weak var textField: UITextField!
+    
+    @IBOutlet private weak var bottomLineView: UIView!
+    @IBOutlet private weak var tipLabel: UILabel!
+
+    @IBOutlet private weak var showPassImageView: UIImageView!
+    @IBOutlet private weak var showStateImageView: UIImageView!
+    @IBOutlet private weak var imageWidthConstraint: NSLayoutConstraint!
+    @IBOutlet private weak var centerBottomConstraint: NSLayoutConstraint!
+    @IBOutlet private weak var centerTopConstraint: NSLayoutConstraint!
+
+    @IBOutlet private weak var tipImageView: UIImageView!
+    @IBOutlet private weak var showPasswordLabel: UILabel!
+
+    @IBOutlet private weak var usernameLeadingConstraint: NSLayoutConstraint!
+    @IBOutlet private weak var bottomStackView: UIStackView!
+
+
+
+    var didTapReturn: (() -> Void)?
+    var didTapIcon: (() -> Void)?
+    var hasText: ((Bool) -> Void)?
+    var didSelectPickerIndex: ((Int) -> Void)?
+    var shouldBeginEditing: (() -> Bool)?
+
+    // Variables
+    let datePicker = UIDatePicker()
+    let pickerView = UIPickerView()
+    var selectionArray: [String] = []
+    var shouldScalePlaceholder = true
+    var isSelect: Bool = false
+    var isCurrency: Bool = false
+    var isTipPermanent: Bool = false
+
+    var showingTipLabel: Bool = false
+
+    var hasCustomRightLabel: Bool = false
+
+
+
+
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        self.setup()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+
+        self.setup()
+    }
+
+    func setup() {
+
+        if textField.text != "" {
+            self.slideUp()
+        }
+
+        containerView.backgroundColor = UIColor.App2.backgroundSecondary
+        containerView.layer.cornerRadius = CornerRadius.headerInput
+
+        containerView.layer.borderWidth = 1
+        containerView.layer.borderColor = UIColor.App2.backgroundSecondary.cgColor
+        
+        self.textField.autocorrectionType = .no
+        
+
+        self.showPassImageView.isUserInteractionEnabled = true
+        self.showStateImageView.isUserInteractionEnabled = true
+
+        self.showStateImageView.isHidden = true
+        self.showPassImageView.isHidden = true
+
+
+
+        tipImageView.isHidden = true
+
+        self.headerPlaceholderLabel.alpha = 0.0
+
+        self.bottomLineView.isHidden = true
+
+        let text = localized("show")
+
+    }
+
+    func shouldSlideDown() -> Bool {
+        if let text = textField.text, !text.isEmpty {
+            return false
+        }
+        return true
+    }
+
+    func slideUp(animated: Bool = true) {
+
+        if headerLabel.text?.isEmpty ?? true {
+            return
+        }
+
+        self.centerBottomConstraint.isActive = false
+        self.centerTopConstraint.isActive = true
+
+        UIView.animate(withDuration: animated ? 0.2 : 0, delay: 0.0, options: .curveEaseOut) {
+            self.layoutIfNeeded()
+
+            if self.shouldScalePlaceholder {
+                let movingWidthDiff = (self.headerLabel.frame.size.width - (self.headerLabel.frame.size.width * 0.8)) / 2
+                self.headerLabel.transform = CGAffineTransform(scaleX: 0.8, y: 0.8).concatenating(CGAffineTransform(translationX: -movingWidthDiff, y: 0))
+            }
+            self.shouldScalePlaceholder = false
+        } completion: { _ in
+
+        }
+    }
+
+    func slideDown() {
+
+        if headerLabel.text?.isEmpty ?? true {
+            return
+        }
+
+        self.centerBottomConstraint.isActive = true
+        self.centerTopConstraint.isActive = false
+
+        UIView.animate(withDuration: 0.2, delay: 0.0, options: .curveEaseOut) {
+            self.layoutIfNeeded()
+            self.headerLabel.transform = CGAffineTransform.identity
+            self.shouldScalePlaceholder = true
+        } completion: { _ in
+
+        }
+    }
+
+    override var canBecomeFirstResponder: Bool {
+        return true
+    }
+
+    @discardableResult
+    override func resignFirstResponder() -> Bool {
+        super.resignFirstResponder()
+        self.textField.resignFirstResponder()
+        return true
+    }
+
+    func setText(_ text: String, slideUp: Bool = true) {
+        self.textField.text = text
+        if slideUp {
+            self.slideUp(animated: false)
+        }
+    }
+
+    func setPlaceholderText(_ placeholder: String) {
+        self.textField.placeholder = ""
+        self.headerLabel.text = placeholder
+    }
+  
+
+    func setPlaceholderColor(_ color: UIColor) {
+        self.headerLabel.textColor = color
+    }
+
+    func setSecureField(_ isSecure: Bool) {
+        self.isSecureField = isSecure
+    }
+
+    func showPasswordLabelVisible(visible: Bool) {
+        self.showPasswordLabel.isHidden = !visible
+    }
+
+    func setRightLabelCustom(title: String, font: UIFont, color: UIColor) {
+        self.hasCustomRightLabel = true
+        self.showPasswordLabel.attributedText = nil
+        self.showPasswordLabel.text = title
+        self.showPasswordLabel.font = font
+        self.showPasswordLabel.textColor = color
+    }
+
+    func setHeaderLabelColor(_ color: UIColor) {
+        self.headerLabel.textColor = color
+    }
+
+    func setTextFieldColor(_ color: UIColor) {
+        self.textField.textColor = color
+    }
+
+    func setViewColor(_ color: UIColor) {
+        self.containerView.backgroundColor = color
+    }
+
+    func setViewBorderColor(_ color: UIColor) {
+        self.containerView.layer.borderColor = color.cgColor
+    }
+
+    func setImageTextField(_ image: UIImage, size: CGFloat = 30) {
+        self.showStateImageView.image = image
+        self.showStateImageView.isHidden = false
+        self.imageWidthConstraint.constant = size
+
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(didTapIconImageVIew(_:)))
+
+        self.showStateImageView.isUserInteractionEnabled = true
+        self.showStateImageView.addGestureRecognizer(tapGestureRecognizer)
+    }
+
+    @objc func didTapIconImageVIew(_ sender: AnyObject) {
+
+        didTapIcon?()
+    }
+
+    func setTextFieldDefaultValue(_ value: String) {
+        self.textField.text = value
+        self.slideUp(animated: false)
+    }
+
+    func setTextFieldFont(_ font: UIFont) {
+        self.textField.font = font
+    }
+
+    func setHeaderLabelFont(_ font: UIFont) {
+        self.headerLabel.font = font
+    }
+
+    func setKeyboardType(_ keyboard: UIKeyboardType) {
+        self.textField.keyboardType = keyboard
+    }
+
+    func setDatePickerMode() {
+        datePicker.datePickerMode = .date
+        if #available(iOS 13.4, *) {
+            datePicker.preferredDatePickerStyle = .wheels
+        }
+        datePicker.addTarget(self, action: #selector(self.dateChanged), for: .allEvents)
+
+        let doneButton = UIBarButtonItem.init(title: localized("done"), style: .done, target: self, action: #selector(self.datePickerDone))
+
+        let toolBar = UIToolbar()
+        toolBar.sizeToFit()
+        toolBar.setItems([UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: nil, action: nil), doneButton], animated: true)
+
+        textField.inputAccessoryView = toolBar
+        textField.inputView = datePicker
+    }
+
+    @objc func datePickerDone() {
+        textField.resignFirstResponder()
+    }
+
+    @objc func dateChanged() {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        let selectedDate = dateFormatter.string(from: datePicker.date)
+        textField.text = "\(selectedDate)"
+    }
+
+    func setPickerArray(_ array: [String]) {
+        selectionArray = array
+        pickerView.selectRow(0, inComponent: 0, animated: true)
+        textField.text = selectionArray[0]
+    }
+
+    func setSelectedPickerOption(option: Int) {
+        pickerView.selectRow(option, inComponent: 0, animated: true)
+        textField.text = selectionArray[option]
+    }
+
+    func setSelectionPicker(_ array: [String], headerVisible: Bool = false, defaultValue: Int = 0) {
+        selectionArray = array
+
+        pickerView.delegate = self
+        //pickerView.selectRow(defaultValue, inComponent: 0, animated: true)
+
+        if !headerVisible {
+            headerLabel.isHidden = true
+        }
+        else {
+            slideUp(animated: false)
+        }
+
+        textField.inputView = pickerView
+        textField.text = selectionArray[defaultValue]
+        
+
+        // Set arrow image
+        let arrowDropdownImageView = UIImageView()
+        arrowDropdownImageView.frame = CGRect(x: 0, y: 0, width: 20, height: 20)
+        let arrowImageView = UIImageView(image: UIImage(named: "selector_arrow_down_icon"))
+        arrowImageView.frame = CGRect(x: -20, y: -4, width: 10, height: 10)
+        arrowImageView.contentMode = .scaleAspectFit
+        arrowDropdownImageView.addSubview(arrowImageView)
+        textField.rightView = arrowDropdownImageView
+        textField.rightViewMode = .always
+
+        dismissPickerView()
+    }
+
+    func dismissPickerView() {
+        let toolBar = UIToolbar()
+        toolBar.sizeToFit()
+        let button = UIBarButtonItem(title: localized("done"), style: .plain, target: self, action: #selector(pickerAction))
+
+        toolBar.setItems([UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: nil, action: nil), button], animated: true)
+        toolBar.isUserInteractionEnabled = true
+        textField.inputAccessoryView = toolBar
+    }
+
+    @objc func pickerAction() {
+        self.endEditing(true)
+    }
+
+    func showErrorOnField(text: String, color: UIColor = .systemRed) {
+
+        self.tipLabel.text = text
+        self.tipLabel.textColor = color
+
+        self.fieldState = .error
+
+        UIView.animate(withDuration: 0.1) {
+            self.tipLabel.alpha = 1.0
+        }
+
+        self.containerView.layer.borderColor = color.cgColor
+
+        self.showingTipLabel = true
+    }
+
+    func showTip(text: String, color: UIColor = .systemRed) {
+
+        tipLabel.text = text
+        tipLabel.textColor = color
+
+        UIView.animate(withDuration: 0.1) {
+            self.tipLabel.alpha = 1.0
+        }
+
+        self.showingTipLabel = true
+    }
+
+    func showTipWithoutIcon(text: String, color: UIColor = .systemRed) {
+
+        tipLabel.text = text
+        tipLabel.textColor = color
+
+        UIView.animate(withDuration: 0.1) {
+            self.tipLabel.alpha = 1.0
+        }
+
+        tipImageView.isHidden = true
+
+        self.showingTipLabel = true
+    }
+
+    func hideTipAndError() {
+
+        if !self.showingTipLabel {
+            return
+        }
+
+        showingTipLabel = false
+
+        tipLabel.text = ""
+        tipLabel.textColor = .black
+        containerView.layer.borderColor = highlightColor.cgColor
+        fieldState = .hidden
+
+        UIView.animate(withDuration: 0.1) {
+            self.tipLabel.alpha = 0.0
+        }
+    }
+
+    @objc func didTapShowPassword() {
+        self.shouldShowPassword.toggle()
+    }
+
+}
+
+extension HeaderTextFieldView: UITextFieldDelegate {
+
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        let shouldBeginEditing = self.shouldBeginEditing?() ?? true
+        self.isActive = shouldBeginEditing
+        return shouldBeginEditing
+    }
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+
+        self.isActive = true
+
+        if !isTipPermanent {
+            self.hideTipAndError()
+        }
+
+        if hasCustomRightLabel {
+            self.showPasswordLabel.isHidden = false
+        }
+
+        self.highlightColor = UIColor.App2.textPrimary
+        self.containerView.layer.borderColor = self.highlightColor.cgColor
+
+        self.slideUp()
+
+    }
+
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if self.shouldSlideDown() {
+            self.slideDown()
+        }
+
+        if self.textField.text == "" {
+            self.containerView.layer.borderColor = self.highlightColor.withAlphaComponent(0).cgColor
+
+            if hasCustomRightLabel {
+                self.showPasswordLabel.isHidden = true
+            }
+        }
+
+        self.isActive = false
+
+        if isCurrency {
+            textField.text = textField.text?.currencyFormatting()
+        }
+
+        if self.textField.text != "" {
+            hasText?(true)
+        }
+        else {
+            hasText?(false)
+        }
+
+    }
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.didTapReturn?()
+                return true
+    }
+
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        if self.isCurrency {
+            let decimals = CharacterSet(charactersIn: "0123456789.")
+            if range.length>0  && range.location == 0 {
+                return false
+
+            }
+            else if string.rangeOfCharacter(from: decimals) == nil && string != "" {
+                    return false
+            }
+        }
+        return true
     }
 }
 
-struct DropDownSelectionView_Previews: PreviewProvider {
-    static var previews: some View {
-        DropDownSelectionView()
+extension HeaderTextFieldView: UIPickerViewDelegate, UIPickerViewDataSource {
+    // PickerView override methods
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return selectionArray.count
+    }
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        self.didSelectPickerIndex?(row)
+        return selectionArray[row]
+    }
+
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let selectedItem = selectionArray[row]
+        textField.text = selectedItem
     }
 }
+
