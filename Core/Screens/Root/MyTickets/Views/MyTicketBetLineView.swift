205c8caa0fde5fca6dc22a498fcfd84c186a4e36
Ruben Roques <rubenroques.nhk@gmail.com>
2022-05-13 10:48:01 +0100

    EveryMatrix and Crocobet new Clients

--- Core/Screens/Root/MyTickets/Views/MyTicketBetLineView.swift
+++ Core/Screens/Root/MyTickets/Views/MyTicketBetLineView.swift
@@ -180,7 +180,7 @@ class MyTicketBetLineView: NibView {
         self.baseView.backgroundColor = UIColor.App.backgroundTertiary
         self.indicatorBaseView.backgroundColor = UIColor.clear
 
-        self.separatorView.backgroundColor = UIColor.App.separatorLineSecondary
+        self.separatorView.backgroundColor = UIColor.App.separatorLine
 
         self.tournamentNameLabel.textColor = UIColor.App.textPrimary
         self.homeTeamNameLabel.textColor = UIColor.App.textPrimary
