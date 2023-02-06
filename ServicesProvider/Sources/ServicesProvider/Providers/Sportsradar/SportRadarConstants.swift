1e5dfe925b2a757bd558d6a8838bf2194d96683a
Ruben Roques <rubenroques.nhk@gmail.com>
2023-02-06 17:27:10 +0000

    New betsson URLs

--- ServicesProvider/Sources/ServicesProvider/Providers/Sportsradar/SportRadarConstants.swift
+++ ServicesProvider/Sources/ServicesProvider/Providers/Sportsradar/SportRadarConstants.swift
@@ -13,23 +13,23 @@ struct SportRadarConstants {
 //       GOMA ENV
 //    =======================================
 
-    //Events via socket
-    static var socketHostname = "wss://velnt-spor-int.optimahq.com"
-    static var socketURL: String {
-        return socketHostname + "/notification/listen/websocket"
-    }
-    static var socketRestHostname = "https://www-sportbook-goma-int.optimahq.com" // To subscribe to contentIDs
-    static var socketLanguageCode = "UK"
-
-    // PAM
-    //static var pamHostname = "https://ps.omegasys.eu"
-    static var pamHostname = "https://bfr-ps.omegasys.eu"
-
-    // Betting
-    static var bettingHostname = "https://www-sportbook-goma-int.optimahq.com"
-
-    // Others
-    static var sportRadarFrontEndURL = "https://cdn1.optimahq.com"
+//    //Events via socket
+//    static var socketHostname = "wss://velnt-spor-int.optimahq.com"
+//    static var socketURL: String {
+//        return socketHostname + "/notification/listen/websocket"
+//    }
+//    static var socketRestHostname = "https://www-sportbook-goma-int.optimahq.com" // To subscribe to contentIDs
+//    static var socketLanguageCode = "UK"
+//
+//    // PAM
+//    //static var pamHostname = "https://ps.omegasys.eu"
+//    static var pamHostname = "https://bfr-ps.omegasys.eu"
+//
+//    // Betting
+//    static var bettingHostname = "https://www-sportbook-goma-int.optimahq.com"
+//
+//    // Others
+//    static var sportRadarFrontEndURL = "https://cdn1.optimahq.com"
 
 //    ===================================================================================================================
 
@@ -40,21 +40,21 @@ struct SportRadarConstants {
 //    =======================================
 
 //    //Events via socket
-//    static var socketHostname = "wss://velnt-bson-ssb-ua.betsson.fr"
-//    static var socketURL: String {
-//        return socketHostname + "/notification/listen/websocket"
-//    }
-//    static var socketRestHostname = "https://www-bson-ssb-uat.betsson.fr" // To subscribe to contentIDs
-//    static var socketLanguageCode = "UK"
-//
-//    // PAM
-//    // //static var pamHostname = "https://ps.omegasys.eu"
-//    static var pamHostname = "https://bfr-ps.omegasys.eu"
-//    // Betting
-//    static var bettingHostname = "https://www-bson-ssb-uat.betsson.fr"
-//
-//    // Others
-//    static var sportRadarFrontEndURL = "https://cdn1.optimahq.com"
+    static var socketHostname = "wss://velnt-bson-ssb-ua.betsson.fr"
+    static var socketURL: String {
+        return socketHostname + "/notification/listen/websocket"
+    }
+    static var socketRestHostname = "https://velsv-bson-ssb-ua.betsson.fr" // To subscribe to contentIDs
+    static var socketLanguageCode = "UK"
+
+    // PAM
+    // //static var pamHostname = "https://ps.omegasys.eu"
+    static var pamHostname = "https://bfr-ps.omegasys.eu"
+    // Betting
+    static var bettingHostname = "https://velsv-bson-ssb-ua.betsson.fr"
+
+    // Others
+    static var sportRadarFrontEndURL = "https://cdn1.optimahq.com"
 
 //    ===================================================================================================================
 
