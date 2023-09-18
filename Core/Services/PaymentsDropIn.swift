a6175fd18c67026e478368f971293934d731a8ef
Ruben Roques <rubenroques.nhk@gmail.com>
2023-09-18 10:29:37 +0100

    New target for betsson prod

--- Core/Services/PaymentsDropIn.swift
+++ Core/Services/PaymentsDropIn.swift
@@ -88,18 +88,19 @@ class PaymentsDropIn {
 
                 self.payment = payment
 
+                let context = AdyenContext(apiContext: apiContext, payment: payment)
                 let adyenSessionConfiguration = AdyenSession.Configuration(sessionIdentifier: sessionId,
                                                                            initialSessionData: sessionData,
-                                                                           context: AdyenContext(apiContext: apiContext, payment: payment))
+                                                                           context: context)
 
                 AdyenSession.initialize(with: adyenSessionConfiguration, delegate: self, presentationDelegate: self) { [weak self] result in
                         switch result {
                         case let .success(session):
-                            //Store the session object.
+                            // Store the session object.
                             self?.adyenSession = session
                             self?.hasSessionInitialized.send(true)
                         case let .failure(error):
-                            //Handle the error.
+                            // Handle the error.
                             print("ADYEN SESSION INIT FAILURE: \(error)")
                         }
                     }
@@ -162,7 +163,6 @@ class PaymentsDropIn {
 
         print("AMOUNT DEPOSIT: \(amount)")
 
-        // TODO: ADYEN_IDEAL -> ADYEN_CARD
         Env.servicesProvider.processDeposit(paymentMethod: "ADYEN_CARD", amount: amount, option: "DROP_IN")
             .receive(on: DispatchQueue.main)
             .sink(receiveCompletion: { [weak self] completion in
@@ -232,21 +232,18 @@ class PaymentsDropIn {
 
                 // Optional Payment
                 let payment = Payment(amount: Amount(value: Int(self.dropInDepositAmount) ?? 0, currencyCode: "EUR"), countryCode: "FR")
-
                 let adyenContext = AdyenContext(apiContext: apiContext, payment: payment)
 
                 // Without session payments
-//                let dropInConfiguration = DropInComponent.Configuration()
-//
-//                dropInConfiguration.card.allowedCardTypes = [.visa, .masterCard, .carteBancaire]
-//
-//                let dropInComponent = DropInComponent(paymentMethods: paymentMethods, context: adyenContext, configuration: dropInConfiguration)
-
-                // With session payments
-                let dropInComponent = DropInComponent(paymentMethods: session.sessionContext.paymentMethods, context: adyenContext)
-
+                let dropInConfiguration = DropInComponent.Configuration()
+                // dropInConfiguration.card.allowedCardTypes = [.visa, .masterCard, .carteBancaire]
+                let dropInComponent = DropInComponent(paymentMethods: paymentMethods, context: adyenContext, configuration: dropInConfiguration)
                 dropInComponent.delegate = self.adyenSession
-
+                
+                // With session payments
+                // let dropInComponent = DropInComponent(paymentMethods: session.sessionContext.paymentMethods, context: adyenContext)
+                // dropInComponent.delegate = self.adyenSession
+                
                 self.dropInComponent = dropInComponent
 
                 return dropInComponent
@@ -397,3 +394,4 @@ extension PaymentsDropIn: DropInComponentDelegate, AdyenSessionDelegate, Present
 
     }
 }
+
