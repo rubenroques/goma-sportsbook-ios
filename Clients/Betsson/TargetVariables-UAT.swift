539a3b6a0da94c2b89b51bdf9e213f4bf3b794cc
Ruben Roques <ruben@gomadevelopment.pt>
2025-02-21 15:36:21 +0000

    version bump

--- Clients/Betsson/TargetVariables-UAT.swift
+++ Clients/Betsson/TargetVariables-UAT.swift
@@ -60,7 +60,7 @@ struct TargetVariables: SportsbookTarget {
     }
 
     static var features: [SportsbookTargetFeatures] {
-        return [.cashback]
+        return [.cashback, .legalAgeWarning]
     }
 
     static var shouldUserBlurEffectTabBar: Bool {
