fa78a36400806ce2fda5e411f653f531c18b8515
Ruben Roques <ruben@gomadevelopment.pt>
2026-01-05 21:26:33 +0000

    test(GomaUI): add snapshot tests for EmptyStateActionView, ExpandableSectionView, ExtendedListFooterView

---
+++ Frameworks/GomaUI/GomaUI/Tests/GomaUITests/SnapshotTests/ExpandableSectionView/ExpandableSectionViewSnapshotTests.swift
@@ -0,0 +1,46 @@
+import XCTest
+import SnapshotTesting
+@testable import GomaUI
+
+final class ExpandableSectionViewSnapshotTests: XCTestCase {
+
+    // MARK: - Basic States
+
+    func testExpandableSectionView_BasicStates_Light() throws {
+        let vc = ExpandableSectionViewSnapshotViewController(category: .basicStates)
+        assertSnapshot(
+            of: vc,
+            as: .image(on: SnapshotTestConfig.device, size: SnapshotTestConfig.size, traits: SnapshotTestConfig.lightTraits),
+            record: SnapshotTestConfig.record
+        )
+    }
+
+    func testExpandableSectionView_BasicStates_Dark() throws {
+        let vc = ExpandableSectionViewSnapshotViewController(category: .basicStates)
+        assertSnapshot(
+            of: vc,
+            as: .image(on: SnapshotTestConfig.device, size: SnapshotTestConfig.size, traits: SnapshotTestConfig.darkTraits),
+            record: SnapshotTestConfig.record
+        )
+    }
+
+    // MARK: - Content Variants
+
+    func testExpandableSectionView_ContentVariants_Light() throws {
+        let vc = ExpandableSectionViewSnapshotViewController(category: .contentVariants)
+        assertSnapshot(
+            of: vc,
+            as: .image(on: SnapshotTestConfig.device, size: SnapshotTestConfig.size, traits: SnapshotTestConfig.lightTraits),
+            record: SnapshotTestConfig.record
+        )
+    }
+
+    func testExpandableSectionView_ContentVariants_Dark() throws {
+        let vc = ExpandableSectionViewSnapshotViewController(category: .contentVariants)
+        assertSnapshot(
+            of: vc,
+            as: .image(on: SnapshotTestConfig.device, size: SnapshotTestConfig.size, traits: SnapshotTestConfig.darkTraits),
+            record: SnapshotTestConfig.record
+        )
+    }
+}
